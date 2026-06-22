423025b6749994b829a6447908e41af96338a8f8
Bartek <bartek@MacBook-Air-Bartek.local>
2026-06-22 19:10:36 +0200

    Revert "Perf: move full wiggle chain build to useEffect, fast no-wiggle corridors"
    This reverts commit a5f10775d853b67c6acd2405162823f746ce9f9b.

--- frontend/src/lib/riverChains.ts
+++ frontend/src/lib/riverChains.ts
@@ -396,9 +396,7 @@ export function buildRiverChainsV2(
     const hasSegWiggle = sp?.wiggleAmp !== undefined || sp?.wiggleFreq !== undefined
     const hasAnyOverride = hasSegWiggle || hopKeysList.some(k => hopProps[k]?.wiggleAmp !== undefined || hopProps[k]?.wiggleFreq !== undefined)
     let chain: [number, number][]
-    if (globalAmp === 0 && !hasAnyOverride) {
-      chain = baseChain
-    } else if (!hasAnyOverride) {
+    if (!hasAnyOverride) {
       chain = wiggleChain(baseChain, globalAmp, globalFreq, cosLat)
     } else {
       const dense = [...baseChain] as [number, number][]
